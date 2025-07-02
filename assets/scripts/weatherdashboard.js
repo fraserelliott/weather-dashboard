c6c1d6e0ff45eb9d57a858f3ad3d3f5d991d164b
Fraser Elliott <fraserelliott@hotmail.com>
2025-07-02 21:41:35 +0100

    implemented function to update carousel's data with 5 day forecast

--- assets/scripts/weatherdashboard.js
+++ assets/scripts/weatherdashboard.js
@@ -7,6 +7,7 @@ document.getElementById("locationInput").addEventListener("keydown", (event) => 
 });
 
 const API_KEY = "f23ee9deb4e1a7450f3157c44ed020e1";
+const WEEKDAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 function getLocation() {
     const location = document.getElementById("locationInput").value;
@@ -36,18 +37,24 @@ function getWeather(lat, lon) {
 }
 
 function updateWeather(obj) {
-    console.log(`Weather data: ${obj}`);
+    console.log("weather data", obj);
 
-    const date = new Date();
-    document.getElementById("currentTime").textContent = `${date.getHours()}:${date.getMinutes()}`;
+    const date = new Date(obj.dt * 1000);
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+
+    document.getElementById("currentTime").textContent = `${hours}:${minutes} - ${toTitleCase(document.getElementById("locationInput").value)}`;
     document.getElementById("currentTemperature").textContent = `${Number.parseInt(obj.main.temp)}\u00B0C`;
     document.getElementById("weatherSummary").textContent = obj.weather[0].main;
     document.getElementById("feelsLikeTemperature").textContent = `Feels like ${Number.parseInt(obj.main.feels_like)}\u00B0C`;
-    document.getElementById("weatherDescription").textContent = `${obj.weather[0].description}. The high will be ${obj.main.temp_max}\u00B0C`;
+    document.getElementById("weatherDescription").textContent = `${toTitleCase(obj.weather[0].description)}. The high will be ${Number.parseInt(obj.main.temp_max)}\u00B0C`;
     document.getElementById("windSpeed").textContent = `${Number.parseFloat(obj.wind.speed).toFixed(1)} km/h`;
     document.getElementById("humidity").textContent = `${obj.main.humidity}%`;
-    document.getElementById("visibility").textContent = `${Number.parseFloat(obj.visibility/1000).toFixed(1)}km`;
+    document.getElementById("visibility").textContent = `${Number.parseFloat(obj.visibility / 1000).toFixed(1)}km`;
     document.getElementById("pressure").textContent = `${obj.main.pressure}hPa`;
+    document.getElementById("weatherIcon").src = `https://openweathermap.org/img/wn/${obj.weather[0].icon}@2x.png`;
+
+    getForecast(obj.coord.lat, obj.coord.lon);
 }
 
 function getForecast(lat, lon) {
@@ -58,14 +65,15 @@ function getForecast(lat, lon) {
 }
 
 function updateForecast(obj) {
-    console.log(`Forecast data: ${obj}`);
-    
+    console.log("forecast data", obj);
+
     const forecastChartData = []; //Array where forecastChartData[0] is today, 1 is tomorrow, etc. For charts.
     const forecastData = []; //Like chart data but recording data used for the daily summary
 
     let i = 0; //Start at 0
     let lastDay = new Date().getDate();
     forecastChartData[0] = [];
+    forecastData[0] = [];
     obj.list.forEach((item) => {
         const dt = new Date(item.dt * 1000); //convert from unix datetime
         const currentDay = dt.getDate();
@@ -76,7 +84,7 @@ function updateForecast(obj) {
             forecastData[i] = [];
             lastDay = currentDay;
         }
-        
+
         forecastChartData[i].push({
             x: dt.getHours(),
             y: item.main.temp
@@ -98,8 +106,15 @@ function updateForecast(obj) {
     updateForecastElements(forecastChartData, forecastSummary);
 }
 
-function updateForecastElements(forecastChartData, forecastSummary) {
-
+function updateForecastElements(forecastChartData, forecastSummary) {   
+    for(let i=1; i<=5; i++) {
+        let index=i-1;
+        document.getElementById(`cardday${i}`).textContent = WEEKDAYS[forecastSummary[index].day];
+        document.getElementById(`cardhigh${i}`).textContent = `High: ${Number.parseInt(forecastSummary[index].high)}\u00B0C`;
+        document.getElementById(`cardlow${i}`).textContent = `Low: ${Number.parseInt(forecastSummary[index].low)}\u00B0C`;
+        document.getElementById(`cardhumid${i}`).textContent = `Humidity: ${Number.parseInt(forecastSummary[index].humidity)}%`;
+        document.getElementById(`cardwind${i}`).textContent = `Wind Speed: ${Number.parseFloat(forecastSummary[index].wind).toFixed(1)} km/h`;
+    }
 }
 
 function summariseForecast(forecastData) {
@@ -127,11 +142,13 @@ function summariseDay(forecastDay) {
         low,
         high,
         humidity: humiditySum / forecastDay.length,
-        wind: windSum / forecastDay.length
+        wind: windSum / forecastDay.length,
+        day: forecastDay[0].dt.getDay()
     };
 }
 
 function createAlert(text, delayMs) {
+    console.log(`Creating alert: ${text}`);
     const alert = document.createElement("div");
     alert.className = "erroralert";
     alert.innerHTML = `${text}`;
@@ -141,4 +158,12 @@ function createAlert(text, delayMs) {
         alert.classList.add('hide');
         setTimeout(() => document.body.removeChild(alert), 150);
     }, delayMs);
+}
+
+function toTitleCase(str) {
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
 }
